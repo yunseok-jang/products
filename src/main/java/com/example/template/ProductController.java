87234e763955344a658281a342213b98aedacee3
yunseok-jang <58927578+yunseok-jang@users.noreply.github.com>
2019-12-16 16:11:45 +0900

    Update ProductController.java

--- src/main/java/com/example/template/ProductController.java
+++ src/main/java/com/example/template/ProductController.java
@@ -10,7 +10,7 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("/item/{productId}")
-    Product productStockCheck(@PathVariable(value = "productId") Long productId) {
+    Product itemStockCheck(@PathVariable(value = "productId") Long productId) {
 
         System.out.println("productStockCheck call");
         try {
